aa0ceabf9a98c99815aee4f58bf2241a099ce980
Bill Carlisle <bcarlisl@gmail.com>
2015-08-14 23:15:42 -0400

    Reformatted comments and added URL to CodeEval problem.

--- Fibonacci_Series/n-fib.lua
+++ Fibonacci_Series/n-fib.lua
@@ -1,7 +1,10 @@
 --[[
-Calculate nth Fibonacci number recursively since we only care about the result
-and don't need to do anything with the numbers in between. Stack safe as Lua
-implements proper tail calls.
+Challenge description: https://www.codeeval.com/open_challenges/22/
+
+solution description:
+Calculate nth Fibonacci number. Since we only care about the result and don't
+need to do anything with the other numbers in the sequence, we use recursion,
+which is stack safe as Lua implements proper tail calls.
 --]]
 
 function nth_fib (n, fib_sub_n, fib_sub_n_minus_one)
