8dc672c4f5a3e01cbfde798f3719055659dfc619
Bill Carlisle <bcarlisl@gmail.com>
2015-10-14 13:28:58 -0400

    Additional optimized function for edge cases where x and/or y = 1
    Added function one_fizzbuzz() to optimize FizzBuzz algorithm in the
    case that one or both divisors is equal to 1.

--- FizzBuzz/FizzBuzz.lua
+++ FizzBuzz/FizzBuzz.lua
@@ -7,6 +7,56 @@ divisor is an even divisor of the other or where the divisors are equal.
 --]]
 
 --[[
+In the case that one divisor equals one, we can optimize FizzBuzz by only
+checking one divisor for even divisibility. In the case that both are equal
+to 1, no checks are necessary apart from a bounds checks to prevent trailing
+spaces per the exercise instructions.
+
+While these are technically different edge cases, they are similar enough to
+justify putting this secondary check into one_fizzbuzz() rather than having a
+second check and additional subroutine.
+--]]
+
+local function one_fizzbuzz(x, y, limit)
+  if x == 1 then
+    if y == 1 then
+      for i=1, limit do
+        io.write("FB")
+        if i == limit then
+          return
+    		end
+    		io.write(" ")
+    	end
+    else
+      for i = 1, limit do
+        if i % y == 0 then
+          io.write("FB")
+        else
+          io.write("F")
+        end
+        if i == limit then
+          return
+        end
+        io.write(" ")
+      end
+    end
+  else
+    for i=1, limit do
+      if i % x == 0 then
+        io.write("FB")
+      else
+        io.write("B")
+      end
+      if i == limit then
+        return
+      end
+      io.write(" ")
+    end
+  end
+end
+
+
+--[[
 If x or y is a divisor of the other, we can optimize our FizzBuzz routine by
 doing less checks for divisibility, since we can guarantee that the smaller
 of the two divisors will always be an even divisor of i if the larger divisor
@@ -56,7 +106,7 @@ local function equal_fizzbuzz(x, limit)
   end
 end  
   
-  
+
 -- Generalized form of FizzBuzz
 local function generalized_fizzbuzz(x, y, limit)
   for i=1,limit do
@@ -93,7 +143,9 @@ end
 
 for n, line in ipairs(lines) do
   local x, y, limit = line[1], line[2], line[3]
-  if x ~= y and (x % y == 0 or y % x == 0) then
+  if x == 1 or y == 1 then
+    one_fizzbuzz(x, y, limit)
+  elseif x ~= y and (x % y == 0 or y % x == 0) then
     simplified_fizzbuzz(x, y, limit)
   elseif x == y then
     equal_fizzbuzz(x, limit)
